95b069b61f257c1f7a901e14d4588fce2204d3cf
yangyongbing <1763108660@qq.com>
2022-06-20 23:29:53 +0800

    微信商城小程序商城后台

---
+++ xshop/src/main/java/com/young/xshop/XshopApplication.java
@@ -0,0 +1,13 @@
+package com.young.xshop;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class XshopApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(XshopApplication.class, args);
+    }
+
+}
---
+++ xshop/src/test/java/com/young/xshop/XshopApplicationTests.java
@@ -0,0 +1,13 @@
+package com.young.xshop;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class XshopApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
